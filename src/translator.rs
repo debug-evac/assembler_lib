a87e67c255f4591157d5b6d16d261f65d9d136e9
0julius <0julius@informatik.uni-hamburg.de>
2024-01-11 18:12:09 +0100

    traslator tests

--- src/translator.rs
+++ src/translator.rs
@@ -12,11 +12,11 @@
 use crate::common::{Instruction, Reg, Imm};
 
 fn btype_instr(rs1: Reg, rs2: Reg, imm: Imm) -> u32 {
-   let u12 =  (imm & 0b10_00000_00000i32) >> 1;
-   let l11 = (imm & 0b1_00000_00000i32) >> 11;
-   let upper = (imm & 0b11111_00000i32 | u12) << 25;
-   let lower = (imm & 0b11110i32 | l11) << 7;
-   ((upper + lower) as u32) + ((rs2 as u32) << 20) + ((rs1 as u32) << 15) + 0b1100011_u32
+   let u12 =  (imm & 0b100_00000_00000i32) >> 1;
+   let l11 = (imm & 0b10_00000_00000i32) >> 11;
+   let upper = ((imm & 0b1_11110_00000i32) | u12) << 25;
+   let lower = ((imm & 0b11110i32) | l11) << 7;
+   ((upper + lower) as u32) + ((rs2 as u32) << 21) + ((rs1 as u32) << 16) + 0b1100011_u32
 }
 
 fn stype_instr(rs1: Reg, rs2: Reg, imm: Imm) -> u32 {
@@ -27,11 +27,11 @@ fn stype_instr(rs1: Reg, rs2: Reg, imm: Imm) -> u32 {
 }
 
 fn itype_instr(rd: Reg, rs1: Reg, imm: Imm) -> u32 {
-   ((imm as u32) << 20) + ((rs1 as u32) << 15) + ((rd as u32) << 7)
+   ((imm as u32) << 17) + ((rs1 as u32) << 12) + ((rd as u32) << 7)
 }
 
 fn rtype_instr(rd: Reg, rs1: Reg, rs2: Reg) -> u32 {
-   ((rs2 as u32) << 20) + ((rs1 as u32) << 15) + ((rd as u32) << 7) + 0b0110011_u32
+   ((rs2 as u32) << 17) + ((rs1 as u32) << 15) + ((rd as u32) << 7) + 0b0110011_u32
 }
 
 fn utype_instr(rd: Reg, imm: Imm) -> u32 {
@@ -39,12 +39,12 @@ fn utype_instr(rd: Reg, imm: Imm) -> u32 {
 }
 
 fn jtype_instr(rd: Reg, imm: Imm) -> u32 {
-   let ten_to_one = (imm & 0b11111_11110_i32) << 1;
-   let nineteen_to_twelve = (imm & 0b1111_11110_00000_00000_i32) >> 1;
-   let eleven = (imm & 0b1_00000_00000_i32) >> 2;
-   let twenty = imm & 0b10000_00000_00000_00000_i32;
+   let ten_to_one = (imm & 0b01_11111_11110_i32) << 8;
+   let nineteen_to_twelve = (imm & 0b0_11111_11100_00000_00000_i32) >> 12;
+   let eleven = (imm & 0b010_00000_00000_i32) >> 3;
+   let twenty = imm & 0b01_00000_00000_00000_00000_i32 >> 1;
 
-   (ten_to_one | nineteen_to_twelve | eleven | twenty) as u32 + ((rd as u32) << 7) + 0b1101111_u32
+   (((ten_to_one | nineteen_to_twelve | eleven | twenty) as u32) << 12) + ((rd as u32) << 7) + 0b1101111_u32
 }
 
 impl Instruction {
@@ -129,4 +129,63 @@ pub fn translate(input: Vec<Instruction>) -> Vec<u8> {
    }
 
    output
+
+   
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::borrow::Cow;
+    use crate::{common::{Instruction, Reg, MacroInstr}, linker::link};
+
+    
+    #[test]
+    fn translate_instruction_test (){
+      let mut _inst: Instruction = Instruction::Jal(Reg::G0, 1864135);
+      assert_eq!(0b10011100011011000111000001101111, Instruction::translate_instruction(_inst));
+      
+      _inst = Instruction::Jalr(Reg::G1, Reg::G2, 1138);
+      assert_eq!(0b1000111001000010000011100111, Instruction::translate_instruction(_inst));
+      
+      
+      _inst = Instruction::Beq(Reg::G3, Reg::G4, 3364);
+      println!("{:#032b}", Instruction::translate_instruction(_inst.clone()));
+      println!("{:#032b}", (0b10100100010000011000010011100011 as u32));
+      assert_eq!(0b10100100010000011000010011100011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Bne(Reg::G5, Reg::G6, 3364);
+      assert_eq!(0b10100100011000101001010011100011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Bltu(Reg::G7, Reg::G8, 3364);
+      assert_eq!(0b1010010010000011111001001,Instruction::translate_instruction(_inst));
+      _inst = Instruction::Bge(Reg::G9, Reg::G10, 3364);
+      assert_eq!(0b10100100101001001101010011100011, Instruction::translate_instruction(_inst));
+      
+      _inst = Instruction::Lui(Reg::G11, 757305);
+      assert_eq!(0b10111000111000111001010110110111, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Auipc(Reg::G12, 757305);
+      assert_eq!(0b10111000111000111001011000010111, Instruction::translate_instruction(_inst));
+
+      _inst = Instruction::Addi(Reg::G13, Reg::G14, 2953);
+      assert_eq!(0b10111000100101110000011010010011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Andi(Reg::G15, Reg::G16, 2953);
+      assert_eq!(0b10111000100110000111011110010011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Srli(Reg::G17, Reg::G18, 2953);
+      assert_eq!(0b10111000100110010101100010010011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Sltiu(Reg::G19, Reg::G20, 2953);
+      assert_eq!(0b10111000100110100011100110010011, Instruction::translate_instruction(_inst));
+
+      _inst = Instruction::Sb(Reg::G21, Reg::G22, 2953);
+      assert_eq!(0b10111001010010011000010010100011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Sh(Reg::G23, Reg::G24, 2953);
+      assert_eq!(0b10111001100010111001010010100011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Sw(Reg::G25, Reg::G26, 2953);
+      assert_eq!(0b10111001101011001010010010100011, Instruction::translate_instruction(_inst));
+
+      _inst = Instruction::Addn(Reg::G27, Reg::G28, Reg::G29);
+      assert_eq!(0b00000001110011011000111010110011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::And(Reg::G30, Reg::G31, Reg::G0);
+      assert_eq!(0b00000001111111110111000000110011, Instruction::translate_instruction(_inst));
+      _inst = Instruction::Sra(Reg::G1, Reg::G2, Reg::G3);
+      assert_eq!(0b1000000001000001101000110110011, Instruction::translate_instruction(_inst));
+    }
 }

0bd31cb2cf367447dcb9593ccc2b09e4a7480ec1
21sbecker <steven.becker@studium.uni-hamburg.de>
2024-01-20 19:10:49 +0100

    fixed cli interface; moved cli stuff to own function

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,8 @@ use clap::{
     Arg, 
     Command, 
     value_parser, 
-    ArgAction
+    ArgAction,
+    ArgMatches
 };
 use parser::Subroutines;
 use std::{
@@ -31,25 +32,35 @@ use std::{
 
 use crate::common::{LabelRecog, Operation};
 
-fn main() {
-    //let src = "addi $3, $3, 1";
-    let matches = Command::new("Assembler Input")
-    .arg(Arg::new("file")
+fn cli_interface() -> ArgMatches {
+    Command::new("Assembler Input")
+    .arg(Arg::new("input assembly files")
                 .value_parser(value_parser!(PathBuf))
                 .action(ArgAction::Set)
-                .short('f')
+                .short('i')
                 .num_args(1..=10)
                 .required(true)
-                .long("file")
-                .help("A cool file, use \"...\""))
-    .get_matches();
+                .long("input")
+                .help("Input assembly files, use \"<PATH>\""))
+    .arg(Arg::new("destination binary file")
+                .value_parser(value_parser!(PathBuf))
+                .action(ArgAction::Set)
+                .short('d')
+                .num_args(1)
+                .required(false)
+                .long("destination")
+                .help("The destination for the output file"))
+    .get_matches()
+}
 
-    let vals: Vec<&PathBuf> = matches.get_many::<PathBuf>("file")
+fn main() {
+    let matches = cli_interface();
+
+    let vals: Vec<&PathBuf> = matches.get_many::<PathBuf>("input assembly files")
     .expect("`file`is required")
     .collect();
     
     let mut parsed_vector: Vec<(LabelRecog, Vec<Operation>)> = vec![];
-
     let mut string_vector: Vec<String> = vec![];
 
     for file in vals {
@@ -86,60 +97,16 @@ fn main() {
     let optimized_code = optimizer::optimize(linked_vector);
     let translated_code = translator::translate(optimized_code);
 
-    let assembler_output = Command::new("Assembler Output")
-    .arg(Arg::new("destination")
-                .value_parser(value_parser!(PathBuf))
-                .action(clap::ArgAction::Append)
-                .short('d')
-                .num_args(1..=10)
-                .required(false)
-                .long("destination")
-                .help("The destination for the output file"))
-    .get_matches();
-
-    let filename = match assembler_output.get_one::<PathBuf>("destination"){
+    let outpath = match matches.get_one::<PathBuf>("destination binary file"){
         None => Path::new("./a.bin"),
         Some(assembler_output) => assembler_output,
     };
 
-    let mut output_file = match File::create(filename) {
+    let mut output_file = match File::create(outpath) {
         Ok(file) => file,
         Err(msg) => panic!("[Error] could not create output file: {}", msg),
     };
 
     output_file.write_all(&translated_code).expect("[Error] could not write to output file!");
-
-         /*
- let myfile: Vec<&PathBuf> = matches.get_many::<PathBuf>("file")
- .expect("files are needed!")
- .collect();
- println!("{:?}", myfile); 
- 
-     
-     let source_code = r#"START:
-     movu $3, 16
-     movl $3, 16
-     cmpe $3, $4
-     bt END
-     movu $4, 16
-     movl $4, 16
-     jmp START
- END:
- "#;
- 
-     let res = parser::parse("divi $10,$10, 51");
-     match res {
-         Ok(parsed) => println!("{:?}", parsed.1[0]),
-         Err(mes) => println!("{}", mes),
-     };
- 
-     let res = parser::parse(source_code);
-     match res {
-         Ok(parsed) => println!("{:?}", parsed.1),
-         Err(_) => (),
-     }
- */
-     
- 
 }
  

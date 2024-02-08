2f00dd9a0f5cb0612f340fac07dab0c9174388ca
21sbecker <steven.becker@studium.uni-hamburg.de>
2024-02-08 01:44:33 +0100

    added test cases for new features

--- tests/test_main.rs
+++ tests/test_main.rs
@@ -419,3 +419,148 @@ rep 3, nop
 
     Ok(())
 }
+
+#[test]
+fn test_mif_format_32_cmt() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new()?;
+
+    let input = temp.child("test_mif_format.asm");
+    input.write_str(r#"
+rep 3, nop
+"#)?;
+
+    let cor_output = temp.child("correct_output.mif");
+
+    let mut cor_instr_vec: Vec<u8> = vec![];
+
+    let instr_vec: Vec<u32> = Vec::from([
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011  // nop
+    ]);
+
+    for instr in instr_vec.iter() {
+        cor_instr_vec.extend(instr.to_le_bytes());
+    }
+
+    let mut output_str = "DEPTH = 1024;\nWIDTH = 32;\nADDRESS_RADIX = DEC;\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n".to_string();
+
+    for (counter, values) in cor_instr_vec.chunks_exact(4).enumerate() {
+        output_str.push_str(&format!("{counter}\t: {:08b}{:08b}{:08b}{:08b};\t\t-- {}\n", values[0], values[1], values[2], values[3], "Addi(G0, G0, 0)"));
+    }
+
+    output_str.push_str("END;\n");
+
+    cor_output.write_str(&output_str)?;
+
+    let mut cmd = Command::cargo_bin("assembler")?;
+
+    cmd.arg("-i").arg(input.path()).arg("-o").arg(temp.path().join("a.mif"))
+                 .arg("--width").arg("32").arg("-c");
+    cmd.assert()
+        .success();
+
+    temp.child("a.mif")
+        .assert(predicate::path::is_file())
+        .assert(predicate::path::eq_file(cor_output.path()));
+
+    temp.close()?;
+
+    Ok(())
+}
+
+#[test]
+fn test_mif_format_8_cmt() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new()?;
+
+    let input = temp.child("test_mif_format.asm");
+    input.write_str(r#"
+rep 3, nop
+"#)?;
+
+    let cor_output = temp.child("correct_output.mif");
+
+    let instr_vec: Vec<u32> = Vec::from([
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011  // nop
+    ]);
+
+    let mut output_str = "DEPTH = 1024;\nWIDTH = 8;\nADDRESS_RADIX = DEC;\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n".to_string();
+
+    let mut counter: usize = 0;
+
+    for instr in instr_vec.iter() {
+        let mach_instr = instr.to_le_bytes();
+        output_str.push_str(&format!("{counter}\t: {:08b} {:08b} {:08b} {:08b};\t\t-- {}\n", mach_instr[0], mach_instr[1], mach_instr[2], mach_instr[3], "Addi(G0, G0, 0)"));
+        counter += 4;
+    }
+
+    output_str.push_str("END;\n");
+
+    cor_output.write_str(&output_str)?;
+
+    let mut cmd = Command::cargo_bin("assembler")?;
+
+    cmd.arg("-i").arg(input.path()).arg("-o").arg(temp.path().join("a.mif"))
+                 .arg("--width").arg("8").arg("-c");
+    cmd.assert()
+        .success();
+
+    temp.child("a.mif")
+        .assert(predicate::path::is_file())
+        .assert(predicate::path::eq_file(cor_output.path()));
+
+    temp.close()?;
+
+    Ok(())
+}
+
+#[test]
+fn test_debug_format() -> Result<(), Box<dyn std::error::Error>> {
+    let temp = assert_fs::TempDir::new()?;
+
+    let input = temp.child("test_mif_format.asm");
+    input.write_str(r#"
+rep 3, nop
+"#)?;
+
+    let instr_vec: Vec<u32> = Vec::from([
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011, // nop
+        0b00_00000_00000_00000_00000_00000_10011  // nop
+    ]);
+
+    let mut output_str = "DEPTH = 1024;\nWIDTH = 8;\nADDRESS_RADIX = DEC;\nDATA_RADIX = BIN;\nCONTENT\nBEGIN\n".to_string();
+
+    let mut counter: usize = 0;
+
+    for instr in instr_vec.iter() {
+        let mach_instr = instr.to_le_bytes();
+        output_str.push_str(&format!("{counter}\t: {:08b} {:08b} {:08b} {:08b};\t\t-- {}\n", mach_instr[0], mach_instr[1], mach_instr[2], mach_instr[3], "Addi(G0, G0, 0)"));
+        counter += 4;
+    }
+
+    output_str.push_str("END;\n");
+
+    let mut cmd = Command::cargo_bin("assembler")?;
+
+    let no_output_path = temp.path().join("a.mif");
+    let match_str = format!("Debug format chosen! Nothing will be written to {:?}!", no_output_path);
+
+    cmd.arg("-i").arg(input.path()).arg("-o").arg(no_output_path)
+                 .arg("-f").arg("debug").arg("-c");
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::is_empty())
+        .stderr(predicate::str::contains("Comment flag has no effect on debug format!"))
+        .stderr(predicate::str::contains(match_str))
+        .stderr(predicate::str::contains("Emitted 00000000000000000000000000010011 from Addi(G0, G0, 0)"));
+
+    temp.child("a.mif")
+        .assert(predicate::path::missing());
+
+    temp.close()?;
+
+    Ok(())
+}

9dd86a259a3c6d8ebe53008de6c514069864ecaa
21sbecker <steven.becker@studium.uni-hamburg.de>
2024-03-06 16:11:44 +0100

    removed examples, man dir and binary main; changed Cargo.toml

--- examples/assembler_crasher_2000.asm
+++
@@ -1,8 +0,0 @@
-; A very long assembly file
-; Do not use this one for anything other than testing
-; DO NOT USE THE DEBUG LOG LEVEL
-rep 2000000, nop
-rep 2000000, nop
-rep 2000000, nop
-rep 2000000, nop
-rep 2000000, nop
